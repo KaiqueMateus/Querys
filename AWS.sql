4728afd4cb5591667cffa6594049483b6680da82
Kaique Mateus <101655609+KaiqueMateus@users.noreply.github.com>
2025-11-02 12:19:27 -0300

    Create AWS.sql

---
+++ AWS.sql
@@ -0,0 +1,7 @@
+select petronect.filters.cognitoUserId,
+    petronect.filters.tags,
+    petronect.filters.created_at,
+    petronect.filters.deleted_at,
+    petronect.filters.name,
+    petronect.filters.query
+from petronect.filters
